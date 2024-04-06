0d40a7a253f6e1dbeff5935160b4243a9e5b3355
Mari <67670160+The-WindowsVista@users.noreply.github.com>
2024-04-06 01:55:16 -0400

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+
+        // Initialize presence
+        client.user.setPresence({ activities: [{ name: 'Merry Christmas' }], status: 'dnd' });
+
+        // Store the interval ID
+        let presenceInterval = setInterval(() => {
+            client.user.setPresence({ activities: [{ name: 'MariLink v1.0.0 BETA' }], status: 'online' });
+        }, 1000);
+
+        // Clear the interval when necessary (for example, when the bot is shutting down)
+        // You should have a way to clear this interval when it's no longer needed
+        // For example:
+        // process.on('SIGINT', () => {
+        //     clearInterval(presenceInterval);
+        //     process.exit(0);
+        // });
+    },
+};
+
